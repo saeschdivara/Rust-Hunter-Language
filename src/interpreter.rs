7fb2b20b1fad57b668747fc9837e5350597b8ed1
Sascha Häusler <sascha.haeusler@bitvoodoo.ch>
2021-12-25 22:34:10 +0100

    Time source code scanning

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,3 +1,4 @@
+use chrono::Local;
 use log::info;
 use crate::reporting::CodeReporter;
 
@@ -6,7 +7,12 @@ use crate::scanning::Scanner;
 pub fn run(file_name: String, source: String) {
     let reporter = CodeReporter::new();
     let mut scanner = Scanner::new(reporter);
+
+    let start = Local::now().time();
     let tokens = scanner.scan_string(file_name, source);
+    let end = Local::now().time();
+
+    info!("Parsing took {} ms", (end - start).num_milliseconds());
 
     for token in tokens {
         info!("Token: {:?}", token);
